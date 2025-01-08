dbf3b609e878d798ea6ba43dd3242d63ecc0738b
Nirjus Karmakar <128937557+Nirjus@users.noreply.github.com>
2025-01-08 18:48:52 +0530

    Update server.js
    remove the reload function for auto calling the sever

--- server.js
+++ server.js
@@ -81,36 +81,3 @@ io.on("connection", (socket) => {
   });
 });
 
-const url = `${backendUrl}/test`; // Replace with your Render URL
-const interval = 30000; // Interval in milliseconds (30 seconds)
-function reloadWebsite() {
-  fetch(url, {
-    method: "GET", // Specifies the HTTP method
-    headers: {
-      "Content-Type": "application/json", // Example of setting headers
-    },
-  })
-    .then((response) => {
-      if (response.ok) {
-        console.log(
-          `Reloaded at ${new Date().toISOString()}: Status Code ${
-            response.status
-          }`
-        );
-      } else {
-        console.error(
-          `Failed to reload at ${new Date().toISOString()}: Status Code ${
-            response.status
-          }`
-        );
-      }
-    })
-    .catch((error) => {
-      console.error(
-        `Error reloading at ${new Date().toISOString()}:`,
-        error.message
-      );
-    });
-}
-
-setInterval(reloadWebsite, interval);
